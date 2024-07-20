606778eb2de436716bd3d0c08d62a6a1c6ffa415
memento <claude.rc@gmail.com>
2024-07-20 14:40:20 -0500

    (fix) specify rollup in pnpm install (?)

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,14 +9,14 @@ fi
 
 export npm_config_build_from_source=true
 
-rm $PREFIX/bin/node
-ln -s $BUILD_PREFIX/bin/node $PREFIX/bin/node
+rm "${PREFIX}"/bin/node
+ln -s "${BUILD_PREFIX}"/bin/node "${PREFIX}"/bin/node
 
 NPM_CONFIG_USERCONFIG=/tmp/nonexistentrc
 
 pnpm import
-pnpm install
+pnpm install rollup
 
 pnpm pack
-npm install -g ${PKG_NAME}-${PKG_VERSION}.tgz
+npm install -g "${PKG_NAME}"-"${PKG_VERSION}".tgz
 pnpm licenses list --json | pnpm-licenses generate-disclaimer --json-input --output-file=ThirdPartyLicenses.txt
